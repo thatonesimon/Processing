a3864411de29bf5eb191ef688308022aebe6917f
Simon Wong <thatonesimon@gmail.com>
2018-04-25 15:37:25 -0700

    Song experimentation, circles created on click

--- circles/floating_circles.py
+++ circles/floating_circles.py
@@ -18,8 +18,7 @@ circles = []
 def floating_circles():
     float_setup()
     global circles
-    for i in xrange(num_circles):
-        c = circles[i]
+    for c in circles:
         stroke(c.color.r, c.color.g, c.color.b, 255*c.y/height)
         fill(c.color.r, c.color.g, c.color.b, 255*c.y/height)
         ellipse(c.x, c.y, c.radius, c.radius)
@@ -32,16 +31,28 @@ def float_setup():
     global setup
     if not setup:
         for i in xrange(num_circles):
-            x = random(0, width)
-            start_radius = random(min_rad, max_rad)
-            y = height + start_radius
-            dy = -random(min_dy, max_dy)
-            new_circle = Circle(x, y, start_radius, 0, dy, random(255), random(255), random(255))
-            circles.append(new_circle)
+           create_circle()
         setup = True
         
+def create_circle():
+    x = random(0, width)
+    start_radius = random(min_rad, max_rad)
+    y = height + start_radius
+    dy = -random(min_dy, max_dy)
+    new_circle = Circle(x, y, start_radius, 0, dy, random(255), random(255), random(255))
+    circles.append(new_circle)
+    
+def create_circle(x, y):
+    start_radius = random(min_rad, max_rad)
+    dy = -random(min_dy, max_dy)
+    new_circle = Circle(x, y, start_radius, 0, dy, random(255), random(255), random(255))
+    circles.append(new_circle)
+    
 def reset_circle(c):
     c.x = random(0, width)
     c.radius = random(min_rad, max_rad)
     c.y = height + radius
     c.dy = -random(min_dy, max_dy)
+    
+def mouseClicked():
+    create_circle(mouseX, mouseY)
